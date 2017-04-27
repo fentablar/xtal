ba88fd5acce2c7786573284af3e2b6b3a422984b
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-27 12:31:46 -0700

    add list loop to gen html

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -27,6 +27,13 @@ $(document).ready(function() {
       $("main").append("<div class='board'><h1>" + boardData[bd].name +
       "</h1><div class='board-lists' id='" + boardData[bd].id +
       "'></div></div>");
+      Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
+        for (var ld = 0; ld < listData.length; ld++) {
+          $("#" + listData[ld].idBoard).append("<div class='list'><h2>" +
+          listData[ld].name + "</h2><div class='list-cards' id='" +
+          listData[ld].id + "'></div></div>");
+        }
+      });
     }
   });
 });
