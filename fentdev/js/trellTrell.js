389acd41aa9396590b8abdb6f80abf68c928ccb0
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-27 12:42:50 -0700

    add card loop to gen html

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
           Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
             for (var cd = 0; cd < cardData.length; cd++) {
               $("#" + cardData[cd].idList).append("<div class='card' id='" +
-              cardData[cd].id + "><p>" + cardData[cd].name + "</p></div>");
+              cardData[cd].id + "'><p>" + cardData[cd].name + "</p></div>");
             }
           });
         }
