c16cd3a0bbe5c96098cf15602d6ee25f3dcd2ce8
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-27 11:36:08 -0700

    amend card loop to gen html instead of arrays

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -28,22 +28,20 @@ $(document).ready(function() {
   // authorie Trello app
   trelloAuth();
 
-  // get board,list,card data and push to arrays
+  // get board,list,card data and gen html for each
   Trello.get("members/me/boards", function(boardData) {
     for (var bd = 0; bd < boardData.length; bd++) {
-      boardsArray.push(boardData[bd]);
       $("main").append("<div class='board'><h1>" + boardsArray[bd].name +
       "</h1><div class='board-lists' id='" + boardsArray[bd].id +
       "'></div></div>");
       Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
         for (var ld = 0; ld < listData.length; ld++) {
-          listsArray.push(listData[ld]);
           $("#" + listData[ld].idBoard).append("<div class='list'><h2>" +
           listData[ld].name + "</h2><div class='list-cards' id='" +
-          listData[ld].id + "'></div></div>"); } });
-      Trello.get("boards/" + boardData[bd].id + "/cards", function(cardData) {
-        for (var cd = 0; cd < cardData.length; cd++) {
-          cardsArray.push(cardData[cd]); } });
-    }
-  });
+          listData[ld].id + "'></div></div>");
+          Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
+            for (var cd = 0; cd < cardData.length; cd++) {
+              $("#" + cardData[cd].idList).append("<div class='card' id='" +
+              cardData[cd].id + "><p>" + cardData[cd].name + "</p></div>");
+            } }); } }); } });
 });
