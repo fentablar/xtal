d4d626246f4a3cc5da5562223ef79a0716563773
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-28 15:43:29 -0700

    move id tags fr board-lists to board and list-cards to list

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -3,20 +3,19 @@ var reapTrelloData = function() {
   //get Trello boards, make container, label for each
   Trello.get("members/me/boards", function(boardData) {
     for (var bd = 0; bd < boardData.length; bd++) {
-      $("main").append("<div class='board'><h1>" + boardData[bd].name +
-      "</h1><div class='board-lists' id='" + boardData[bd].id + "'></div></div>");
+      $("main").append("<div class='board' id='" + boardData[bd].id + "'><h1>" + boardData[bd].name + "</h1><div class='board-lists'></div></div>");
 
       //get lists fr each board, make container, label in board for each
       Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
         for (var ld = 0; ld < listData.length; ld++) {
-          $("#" + listData[ld].idBoard).append("<div class='list'><h2>" +
-          listData[ld].name + "</h2><div class='list-cards' id='" +
-          listData[ld].id + "'></div></div>");
+          $("#" + listData[ld].idBoard + " > .board-lists").append("<div class='list' id='" +
+          listData[ld].id + "'><h2>" + listData[ld].name +
+          "</h2><div class='list-cards'></div></div>");
 
           //get cards fr each list, make container, label in list for each
           Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
             for (var cd = 0; cd < cardData.length; cd++) {
-              $("#" + cardData[cd].idList).append("<div class='card' id='" +
+              $("#" + cardData[cd].idList + " > .list-cards").append("<div class='card' id='" +
               cardData[cd].id + "'><p>" + cardData[cd].name + "</p></div>");
             }
           }, function() { console.log("card load failed"); });
