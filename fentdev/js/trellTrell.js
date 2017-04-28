755ab8bd53d254e6bc7a22a2664a4c8aac61296b
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-28 14:30:21 -0700

    revert custom scrollbar on board, list-cards

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -8,14 +8,6 @@ var reapTrelloData = function() {
 
       //get lists fr each board, make container, label in board for each
       Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
-        $(".board").mCustomScrollbar({
-          axis: "x",
-          theme: "inset-2",
-          scrollIntertia: 0,
-          mouseWheel: {
-            enable: false
-          }
-        });
         for (var ld = 0; ld < listData.length; ld++) {
           $("#" + listData[ld].idBoard).append("<div class='list'><h2>" +
           listData[ld].name + "</h2><div class='list-cards' id='" +
@@ -23,11 +15,6 @@ var reapTrelloData = function() {
 
           //get cards fr each list, make container, label in list for each
           Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
-            $(".list-cards").mCustomScrollbar({
-              axis: "y",
-              theme: "minimal-dark",
-              scrollInertia: 0
-            });
             for (var cd = 0; cd < cardData.length; cd++) {
               $("#" + cardData[cd].idList).append("<div class='card' id='" +
               cardData[cd].id + "'><p>" + cardData[cd].name + "</p></div>");
