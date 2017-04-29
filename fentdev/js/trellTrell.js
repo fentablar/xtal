f8d92fbe59093cda78eca83c92459ea062e964f3
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-29 12:01:55 -0700

    attempt to load scrollers via done method

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -1,13 +1,16 @@
 var reapTrelloData = function() {
+  console.log("trell auth success");
 
   //get Trello boards, make container, label for each
   Trello.get("members/me/boards", function(boardData) {
+    console.log("calling boards");
     for (var bd = 0; bd < boardData.length; bd++) {
       $("main").append("<div class='board' id='" + boardData[bd].id + "'><h1>" +
       boardData[bd].name + "</h1><div class='board-lists'></div></div>");
 
       //get lists fr each board, make container, label in board for each
       Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
+        console.log("calling lists");
         for (var ld = 0; ld < listData.length; ld++) {
           $("#" + listData[ld].idBoard + " > .board-lists").append("<div class='list' id='" +
           listData[ld].id + "'><h2>" + listData[ld].name +
@@ -15,6 +18,7 @@ var reapTrelloData = function() {
 
           //get cards fr each list, make container, label in list for each
           Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
+            console.log("calling cards");
             for (var cd = 0; cd < cardData.length; cd++) {
               $("#" + cardData[cd].idList + " > .list-cards").append("<div class='card' id='" +
               cardData[cd].id + "'><p>" + cardData[cd].name + "</p></div>");
@@ -29,6 +33,7 @@ var reapTrelloData = function() {
 var trelloAuthFail = function() { console.log("Trello auth FAIL"); };
 
 var loadScrollers = function() {
+  console.log("load scrollers");
   $(".board").mCustomScrollbar({
     axis: "x",
     theme: "inset-2",
