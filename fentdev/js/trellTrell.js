a5b6135d0baa376074de8188334138d5f562b780
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-28 17:43:55 -0700

    2nd attempt add custom scrollbars to board, list-cards

--- fentdev/js/trellTrell.js
+++ fentdev/js/trellTrell.js
@@ -5,28 +5,6 @@ var reapTrelloData = function() {
     for (var bd = 0; bd < boardData.length; bd++) {
       $("main").append("<div class='board' id='" + boardData[bd].id + "'><h1>" +
       boardData[bd].name + "</h1><div class='board-lists'></div></div>");
-
-      //get lists fr each board, make container, label in board for each
-      Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
-        for (var ld = 0; ld < listData.length; ld++) {
-          $("#" + listData[ld].idBoard + " > .board-lists").append("<div class='list' id='" +
-          listData[ld].id + "'><h2>" + listData[ld].name +
-          "</h2><div class='list-cards'></div></div>");
-
-          //get cards fr each list, make container, label in list for each
-          Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
-            for (var cd = 0; cd < cardData.length; cd++) {
-              $("#" + cardData[cd].idList + " > .list-cards").append("<div class='card' id='" +
-              cardData[cd].id + "'><p>" + cardData[cd].name + "</p></div>");
-            }
-          }, function() { console.log("card load failed"); });
-          $(".list-cards").mCustomScrollbar({
-            axis: "y",
-            theme: "minimal-dark",
-            scrollInertia: 0
-          });
-        }
-      }, function() { console.log("list load failed"); });
       $(".board").mCustomScrollbar({
         axis: "x",
         theme: "inset-2",
@@ -35,6 +13,28 @@ var reapTrelloData = function() {
           enable: false
         }
       });
+
+      //get lists fr each board, make container, label in board for each
+      Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
+        for (var ld = 0; ld < listData.length; ld++) {
+          $("#" + listData[ld].idBoard + " > .board-lists").append("<div class='list' id='" +
+          listData[ld].id + "'><h2>" + listData[ld].name +
+          "</h2><div class='list-cards'></div></div>");
+          $(".list-cards").mCustomScrollbar({
+            axis: "y",
+            theme: "minimal-dark",
+            scrollInertia: 0
+          });
+
+          //get cards fr each list, make container, label in list for each
+          Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
+            for (var cd = 0; cd < cardData.length; cd++) {
+              $("#" + cardData[cd].idList + " > .list-cards").append("<div class='card' id='" +
+              cardData[cd].id + "'><p>" + cardData[cd].name + "</p></div>");
+            }
+          }, function() { console.log("card load failed"); });
+        }
+      }, function() { console.log("list load failed"); });
     }
   }, function() { console.log("board load failed"); });
 };
