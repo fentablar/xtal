562e28cbb76c7cc0a52dc1619b9513599656133b
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-05-01 22:57:49 -0700

    add v0.1 of betaTrelloIntf

---
+++ fentdev/js/betaTrelloIntf.js
@@ -0,0 +1,14 @@
+$(function() {
+  $(".navButton").on("click", function() {
+    $(".navButton").removeClass("navActive");
+    $(this).addClass("navActive");
+    var opt = $(this).attr("id");
+    if(opt === "dashButton") {
+      $("#viewDash").css({"display": "block"});
+      $("#viewBoards").css({"display": "none"});
+    } else {
+      $("#viewDash").css({"display": "none"});
+      $("#viewBoards").css({"display": "block"});
+    }
+  });
+});
