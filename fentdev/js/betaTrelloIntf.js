aa3ef088d395cc3d0f7a59832b84ec6f638c2a28
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-05-02 15:42:43 -0700

    update dash view action items card container template

--- fentdev/js/betaTrelloIntf.js
+++ fentdev/js/betaTrelloIntf.js
@@ -1,3 +1,16 @@
+var trelloAuthFail = function() {
+  console.log("Trello auth FAIL");
+  $("main").append("<div class='board'><h1>Trello authorization error</h1>" +
+  "<br><p>Trello did not authorize&period;&ensp;" +
+  "Please ensure you click &quot;Allow&quot; so the app can connect to " +
+  "Trello&semi; you may need to clear your browser cache before " +
+  "attempting to reauthenticate&period;");
+}
+
+var trelloAuthSuccess = function() {
+  console.log("Trello auth SUCCESS");
+};
+
 $(function() {
   $(".navButton").on("click", function() {
     $(".navButton").removeClass("navActive");
