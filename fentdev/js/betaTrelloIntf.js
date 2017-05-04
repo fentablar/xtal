e01fd75c96fe68089de90592414dac0485a48ed0
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-05-03 22:01:16 -0700

    first attempt at getting boards, lists, cards using deferreds, promises

--- fentdev/js/betaTrelloIntf.js
+++ fentdev/js/betaTrelloIntf.js
@@ -9,23 +9,17 @@ var trelloAuthFail = function() {
 
 var trelloAuthSuccess = function() {
   console.log("Trello auth SUCCESS");
+  $.when(getMe(me))
+  .then(getMyBoards(myBoardsArr))
+  .then(getMyBoardsLists(myBoardsArr, myBoardsListsArr))
+  .then(getMyBoardsCards(myBoardsArr, myBoardsCardsArr))
+  .then(myBoardsHtml(myBoardsArr))
+  .then(myBoardsListsHtml(myBoardsListsArr))
+  .done(myBoardsCardsHtml(myBoardsCardsArr));
 };
 
-function authorizeTrello() {
-  Trello.authorize({
-    type: "popup",
-    name: "xtal trello interface",
-    scope: {
-      read: "true",
-      write: "true"
-    },
-    expiration: "never",
-    success: trelloAuthSuccess,
-    error: trelloAuthFail
-  });
-}
-
 $(function() {
+  var me, myBoardsArr = [], myBoardsListsArr = [], myBoardsCardsArr = [];
   authorizeTrello();
 
   $(".navButton").on("click", function() {
@@ -45,3 +39,99 @@ $(function() {
     $(".actionsCategory").toggleClass("hideMe");
   });
 });
+
+function authorizeTrello() {
+  Trello.authorize({
+    type: "popup",
+    name: "xtal trello interface",
+    scope: {
+      read: "true",
+      write: "true"
+    },
+    expiration: "never",
+    success: trelloAuthSuccess,
+    error: trelloAuthFail
+  });
+}
+
+function getMe(obj) {
+  var dfdMe = $.Deferred();
+  Trello.get("members/me", function(data) {
+    Object.assign(obj, data);
+    dfdMe.resolve();
+  });
+  return dfdMe.promise();
+}
+
+function getMyBoards(boardArr) {
+  var dfdBoard = $.Deferred();
+  Trello.get("members/me/boards", function(boardData) {
+    for (var bd = 0; bd < boardData.length; bd++) {
+      boardArr.push(boardData[bd]);
+    }
+    $.when.apply($, boardArr).then(function() { dfdBoard.resolve(); });
+  });
+  return dfdBoard.promise();
+}
+
+function getMyBoardsLists(boardArr, listArr) {
+  var dfdLists = $.Deferred();
+  for (var i = 0; i < boardArr.length; i++) {
+    Trello.get("boards/" + boardArr[i].id + "/lists", function(listData) {
+      for (var ld = 0; ld < listData.length; ld++) {
+        listArr.push(listData[ld]);
+      }
+    });
+  }
+  $.when.apply($, listArr).then(function() { dfdLists.resolve(); });
+  return dfdLists.resolve();
+}
+
+function getMyBoardsCards(boardArr, cardArr) {
+  var dfdCards = $.Deferred();
+  for (var k = 0; k < boardArr.length; k++) {
+    Trello.get("boards/" + boardArr[k].id + "/cards", function(cardData) {
+      for (var cd = 0; cd < cardData.length; cd++) {
+        cardArr.push(cardData[k]);
+      }
+    });
+  }
+  $.when.apply($, cardArr).then(function() { dfdCards.resolve(); });
+  return dfdCards.promise();
+}
+
+function myBoardsHtml(boardArr) {
+  var dfdBoardHtml = $.Deferred();
+  var tmpArr = [];
+  for (var i = 0; i < boardArr.length; i++) {
+    $("#viewBoards").append("<div class='board' id='" + boardArr[i].id +
+    "'><h1>" + boardArr[i].name + "</h1><div class='board-lists'></div></div>");
+    tmpArr.push(i);
+  }
+  $.when.apply($, tmpArr).then(function() { dfdBoardHtml.resolve(); });
+  return dfdBoardHtml.promise();
+}
+
+function myBoardsListsHtml(listArr) {
+  var dfdListHtml = $.Deferred();
+  var tmpArr = []
+  for (var i = 0; i < listArr.length; i++) {
+    $("#" + listArr[i].idBoard + " > .board-lists").append("<div class='list' id='" +
+    listArr[i].id + "'><h2>" + listArr[i].name +
+    "</h2><div class='list-cards'></div></div>");
+    tmpArr.push(i);
+  }
+  $.when.apply($, tmpArr).then(function() { dfdListHtml.resolve(); });
+  return dfdListHtml.promise();
+}
+
+function myBoardsCardsHtml(cardArr) {
+  var dfdCardHtml = $.Deferred();
+  var tmpArr = [];
+  for (var i = 0; i < cardArr.length; i++) {
+    $("#" + cardArr[i].idList + " > .list-cards").append("<div class='card' id='" +
+    cardArr[i].id + "'><p>" + cardArr[i].name + "</p></div>");
+  }
+  $.when.apply($, tmpArr).then(function() { dfdCardHtml.resolve(); });
+  return dfdCardHtml.promise();
+}
