56c3ab01094fe90c45d643df33a8919bded59e32
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-05-18 21:09:37 -0700

    add hyperlink to board title in boards view

--- fentdev/js/betaTrelloIntf.js
+++ fentdev/js/betaTrelloIntf.js
@@ -129,8 +129,8 @@ function reapMyBoards() {
     var pastDue = [], todayDue = [], futureDue = [], nullDue = [], cmpDue = [];
     for (bh = 0; bh < bhlen; bh++) {
       $("#viewBoards").append("<div class='board' id='" + boardArr[bh].id +
-      "'><h1><a href=" + boardArr[bh].url + ">" + boardArr[bh].name +
-      "</a></h1><div class='board-lists'></div></div>");
+      "'><h1><a href=" + boardArr[bh].url + "target='_blank'>" +
+      boardArr[bh].name + "</a></h1><div class='board-lists'></div></div>");
       $("#boardSelect").append("<option value='" + boardArr[bh].id + "'>" +
       boardArr[bh].name + "</option>");
     }
